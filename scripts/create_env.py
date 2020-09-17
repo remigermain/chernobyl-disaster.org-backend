a71c79b1e3057eee0f09a6639bb308daf678000e
rgermain <germain.remi@protonmail.com>
2020-09-17 11:08:58 +0200

    update script env

--- scripts/create_env.py
+++ scripts/create_env.py
@@ -8,7 +8,7 @@ reg_space = re.compile(REGEX)
 
 
 def main():
-    ENV = {
+    env = {
         "DEBUG": True,
         "SECRET_KEY": f"\"{get_random_secret_key()}\"",
         "SITE_NAME": "",
@@ -25,24 +25,31 @@ def main():
         "SQL_PORT": "",
     }
 
-    env = ENV.copy()
-    open_f = "a" if flag.fusion else "w"  # append or write
-
-    if flag.fusion:
-        with open(".env", "r") as f:
+    if flag.merge:
+        with open(flag.input, "r") as f:
             _file = reg_space.sub(" ", f.read()).split("\n")
             for line in _file:
-                key = line.strip().split("=")[0]
-                if key in env:
-                    del env[key]
+                _split = line.split("=")
+                key = _split[0].strip()
+                value = _split[1].strip() if len(_split) == 2 else None
+                if key[:6] == "export":
+                    key = key[6:].strip()
+                if key in env and value:
+                    env[key] = value
 
-    with open(".env", open_f) as f:
+    with open(flag.output, "w") as f:
         for key, val in env.items():
-            f.write(f"export {key}={val}\n")
+            if flag.export:
+                f.write(f"export {key}={val}\n")
+            else:
+                f.write(f"{key}={val}\n")
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="generate env files")
-    parser.add_argument("-f", "--fusion", action="store_true", help="open existing env file, if key with value exsit, do nothing", default=False)
+    parser.add_argument("-m", "--merge", action="store_true", help="open existing env file, if key with value exsit, do nothing", default=False)
+    parser.add_argument("-o", "--output", help="set the outupt file name", default=".env")
+    parser.add_argument("-i", "--input", help="set the input file name", default=".env")
+    parser.add_argument("-e", "--export", action="store_true", help="ad export prefix", default=False)
     flag = parser.parse_args()
     main()
