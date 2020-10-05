a35854771b490026820f5252d2cb89e13ddd94fc
rgermain <germain.remi@protonmail.com>
2020-10-05 16:02:41 +0200

    update entryp point docker

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 echo "Waiting for postgres..."
 while ! nc -z $POSTGRES_HOST $POSTGRES_PORT; do
-sleep 0.1
+    sleep 0.1
 done
 echo "postgres started"
 
@@ -11,8 +11,6 @@ echo "migrate..."
 ./manage.py migrate --no-input
 echo "collect static..."
 ./manage.py collectstatic --no-input --clear --link
-echo "loaddata..."
-./manage.py loaddata data.json
 echo "fix commit..."
 ./manage.py fix_commit
 echo "generate images..."
