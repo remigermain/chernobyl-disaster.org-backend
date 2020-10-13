8ebc88f99b76d557331cc99221196bffc900b138
rgermain <germain.remi@protonmail.com>
2020-10-13 18:03:17 +0200

    update name db postgre

--- core/settings.py
+++ core/settings.py
@@ -273,7 +273,7 @@ else:
     DATABASES = {
         "default": {
             "ENGINE": os.environ.get("POSTGRES_ENGINE", "django.db.backends.postgresql"),
-            "NAME": os.environ.get("POSTGRES_NAME"),
+            "NAME": os.environ.get("POSTGRES_DB"),
             "USER": os.environ.get("POSTGRES_USER"),
             "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
             "HOST": os.environ.get("POSTGRES_HOST"),
